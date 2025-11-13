e6172180809ee83e60bdb5c69678be1836f2c28e
Miika Valkonen <miika.valkonen08@gmail.com>
2025-11-13 12:57:05 +0200

    Tehtävä 1.7-1.9

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -9,16 +9,34 @@ const Button = (props) => {
 }
 
 const Statistics = (props) => {
-  return (
-    <p>{props.text} {props.value}</p>
-  )
-} 
+  if (props.valueAll > 0) {
+    return (
+      <div>
+        <p>{props.textGood} {props.valueGood}</p>
+        <p>{props.textNeutral} {props.valueNeutral}</p>
+        <p>{props.textBad} {props.valueBad}</p>
+        <p>{props.textAll} {props.valueAll}</p>
+        <p>{props.textAverage} {props.valueAverage}</p>
+        <p>{props.textPositive} {props.valuePositive}</p>
+      </div>
+    )
+  } else {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+  
+}
 
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const total = good + neutral + bad
+  const average = total === 0 ? 0 : (good * 1 + neutral * 0 + bad * -1) / total
+  const positive = total === 0 ? 0 : (good / total) * 100 + "%";
 
   return (
     <div>
@@ -27,9 +45,14 @@ const App = () => {
       <Button action={() => setNeutral(neutral + 1)} text="Neutral"/>
       <Button action={() => setBad(bad + 1)} text="Bad"/>
       <Header name="statistics"/>
-      <Statistics text="Good" value={good}/>
-      <Statistics text="Neutral" value={neutral}/>
-      <Statistics text="Bad" value={bad}/>
+      <Statistics 
+      textGood="Good" valueGood={good}
+      textNeutral="Neutral" valueNeutral={neutral}
+      textBad="Bad" valueBad={bad}
+      textAll="All" valueAll={total}
+      textAverage="Average" valueAverage={average}
+      textPositive="Positive" valuePositive={positive}
+      />
     </div>
   )
 }
