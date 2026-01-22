e32b8fd4e280255124bc855955a02a35da01853a
Miika Valkonen <miika.valkonen08@gmail.com>
2026-01-22 11:44:24 +0200

    Tehtävät 3.13.-3.14

---
+++ osa3/puhelinluettelonbackend/models/person.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+mongoose.set('strictQuery',false)
+
+const url = process.env.MONGODB_URI
+
+console.log("connecting to", url)
+mongoose.connect(url, { family: 4 })
+    .then(result => {
+        console.log("connected to MongoDB")
+    })
+    .catch((error) => {
+        console.log("error connecting to MongoDB:", error.message)
+    })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+personSchema.set("toJSON", {
+    transform: (document, returneOBject) => {
+        returneOBject.id = returneOBject._id.toString()
+        delete returneOBject._id
+        delete returneOBject.__v
+    }
+})
+
+module.exports = mongoose.model("person", personSchema)
