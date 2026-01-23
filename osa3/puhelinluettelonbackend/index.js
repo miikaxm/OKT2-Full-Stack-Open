e8cbbbdc4e4d3c84ccdba6e343eeda5efaefc3da
Miika Valkonen <miika.valkonen08@gmail.com>
2026-01-23 09:38:24 +0200

    Tehtävä 3.16

--- osa3/puhelinluettelonbackend/index.js
+++ osa3/puhelinluettelonbackend/index.js
@@ -5,7 +5,6 @@ const app = express()
 app.use(express.static('dist'))
 app.use(express.json())
 
-
 // Morgan print
 const morgan = require("morgan")
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
@@ -26,23 +25,25 @@ app.get("/api/persons", (request, response) => {
 })
 
 // Get person by id
-app.get("/api/persons/:id", (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+app.get("/api/persons/:id", (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if(person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 // Delete person by id
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
         .then(deletedPerson => {
-            if (deletedPerson) {
-                response.status(204).end()
-            } else {
-                response.status(404).json({ error: "Henkilöä ei löytynyt" })
-            }
+            response.status(204).end()
         })
-        .catch(() => response.status(400).json({ error: "Virheellinen id" }))
+        .catch(error => next(error))
 })
 
 // Create new person
@@ -70,6 +71,26 @@ app.post("/api/persons", (request, response) => {
         response.json(savedPerson)
     })
 })
+
+// Error handle for unknown endpoint
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+// Error handler
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 // Port
 const PORT = process.env.PORT
