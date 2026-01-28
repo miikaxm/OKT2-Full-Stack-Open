db2c92cd53121b7bcf5fd2e0401830220694402b
Miika Valkonen <miika.valkonen08@gmail.com>
2026-01-28 08:32:51 +0200

    4.5

--- osa4/blogilistabackend/utils/list_helper.js
+++ osa4/blogilistabackend/utils/list_helper.js
@@ -5,8 +5,15 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((favorite, current) => {
+    return current.likes > favorite.likes ? current : favorite
+  })
+}
+
   
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
