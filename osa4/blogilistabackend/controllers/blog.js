0a1b5ee635d91367d827eac57021d4e0a72e1998
Miika Valkonen <miika.valkonen08@gmail.com>
2026-02-02 09:56:57 +0200

    Tehätävä 4.17

--- osa4/blogilistabackend/controllers/blog.js
+++ osa4/blogilistabackend/controllers/blog.js
@@ -1,19 +1,38 @@
 const blogRouter = require("express").Router()
 const { response } = require("express")
 const Blog = require("../models/blog")
+const User = require("../models/user")
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+
+  const user = await User.findById(body.userId)
+
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not vlid' })
+  }
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  })
+
   if (!blog.title || !blog.url) {
     return response.status(400).json({ error: 'title or url missing' })
   }
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.status(201).json(savedBlog)
 })
 
