86ace9af54b6108331b8e4f29f30bfb9be5a7412
Miika Valkonen <miika.valkonen08@gmail.com>
2026-02-03 08:34:12 +0200

    5.1

--- osa4/blogilista/src/App.jsx
+++ osa4/blogilista/src/App.jsx
@@ -1,30 +1,74 @@
 import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
 import blogService from './services/blogs'
-import Blogs from './components/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
-    blogService
-      .getAll()
-      .then(response => {
-        setBlogs(response.data)
-      })
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
   }, [])
-  console.log('render', blogs.length, 'blogs')
+
+  const handleLogin = async event => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      console.log('wrong credentials')
+    }
+  }
+  
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label>
+              username
+              <input
+               type="text"
+               value={username}
+               onChange={({ target }) => setUsername(target.value)} 
+              />
+            </label>
+          </div>
+            <div>
+            <label>
+              password
+              <input
+               type="password"
+               value={password}
+               onChange={({ target }) => setPassword(target.value)} 
+              />
+            </label>
+          </div>
+          <button type='submit'>login</button>
+        </form>
+      </div>
+    )
+  }
 
   return (
     <div>
-      <h2>Blogilista</h2>
-      <h3>Blogit</h3>
-
-      <Blogs 
-        blogs={blogs}
-      />
+      <h2>blogs</h2>
+      <h3>{user.name} logged in</h3>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
     </div>
   )
-
 }
 
 export default App
