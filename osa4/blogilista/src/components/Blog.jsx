3c7947d09d719cd06fed6003193924473da975eb
Miika Valkonen <miika.valkonen08@gmail.com>
2026-02-05 08:38:14 +0200

    Tehtävä 5.7

--- osa4/blogilista/src/components/Blog.jsx
+++ osa4/blogilista/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        {blog.title} {blog.author}
+        <button onClick={() => setVisible(true)}>view</button>
+      </div>
+
+      <div style={showWhenVisible}>
+        {blog.title}
+        <button onClick={() => setVisible(false)}>hide</button>
+
+        <div>{blog.url}</div>
+        <div>likes {blog.likes} <button>like</button></div>
+        <div>{blog.author}</div>
+      </div>
+    </div>
+  )
+}
+  
+
 
 export default Blog
