94ad212c8d0f5d7b929e6fd8b32d462555bd65c3
Miika Valkonen <miika.valkonen08@gmail.com>
2026-02-10 13:21:45 +0200

    Tehtävä 5.14

--- osa4/blogilista/src/components/Blog.test.jsx
+++ osa4/blogilista/src/components/Blog.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
-import { use } from 'react'
+import userEvent from '@testing-library/user-event'
 
 test('renders content', () => {
   const blog = {
@@ -21,4 +21,43 @@ test('renders content', () => {
   screen.debug(element)
 
   expect(element).toBeDefined()
+})
+
+test('clicking like calls handler once', async () => {
+  const blog = {
+    title: 'Test',
+    author: 'Miika',
+    url: 'test',
+    likes: "2",
+    user: { username: 'testuser' }
+  }
+
+  const userObj = {
+    username: 'testuser'
+  }
+
+  const mockHandler = vi.fn()
+
+  render(
+    <Blog
+      blog={blog}
+      user={userObj}
+      like={mockHandler}
+    />
+  )
+
+  const user = userEvent.setup()
+
+  // Avaa näkymä
+  await user.click(screen.getByText('view'))
+
+  const url = screen.getByText('test')
+  const likes = screen.getByText("likes 2")
+  const author = screen.getByText('Miika')
+  screen.debug(url)
+  screen.debug(likes)
+  screen.debug(author)
+  expect(url).toBeDefined()
+  expect(likes).toBeDefined()
+  expect(likes).toBeDefined()
 })
