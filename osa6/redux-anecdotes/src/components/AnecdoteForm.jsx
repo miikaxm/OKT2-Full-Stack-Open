c21c62f7204c0a76fdf3b1f0d497c51cbd3f0d10
Miika Valkonen <miika.valkonen08@gmail.com>
2026-03-02 11:45:14 +0200

    Tehtävä 6.15

--- osa6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,15 +1,19 @@
 import { useDispatch } from "react-redux";
 import { newAnecdote } from "../reducers/anecdoteReducer";
 import { setNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-    const addAnecdote = event => {
+    const addAnecdote = async (event) => {
       event.preventDefault()
       const content = event.target.anecdote.value
       event.target.anecdote.value = ''
-      dispatch(newAnecdote(content))
+      const newA = await anecdoteService.createNew(content)
+      dispatch(newAnecdote(newA))
+
+      // Notification of creation of new anecdote with 5 sec cooldown
       dispatch(setNotification(`You added new anecdote '${content}'`))
       setTimeout(() => {
         dispatch(setNotification(''))
