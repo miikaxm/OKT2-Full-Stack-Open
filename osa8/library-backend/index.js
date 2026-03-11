40caa4ba3a662f7e3b0ce452ba13e4badb647d48
Miika Valkonen <miika.valkonen08@gmail.com>
2026-03-11 13:52:33 +0200

    8.5: genren kirjat

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -116,7 +116,7 @@ const typeDefs = /* GraphQL */ `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -126,10 +126,15 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
+      if (!args.author && !args.genre) {
         return books
+      } else if (args.author && args.genre) {
+        return books.filter((b) => b.author === args.author && b.genres.includes(args.genre))
+      } else if (args.author) {
+        return books.filter((b) => b.author === args.author)
+      } else {
+        return books.filter((b) => b.genres.includes(args.genre))
       }
-      return books.filter((b) => b.author === args.author)
     },
     allAuthors: () => authors
   },
