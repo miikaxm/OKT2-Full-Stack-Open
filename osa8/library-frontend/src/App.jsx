3214a8afceb71cf7b702d9d7f9044132b514dc82
Miika Valkonen <miika.valkonen08@gmail.com>
2026-03-17 10:20:53 +0200

    8.18 Kirjautuminen

--- osa8/library-frontend/src/App.jsx
+++ osa8/library-frontend/src/App.jsx
@@ -2,25 +2,55 @@ import { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import LoginForm from './components/LoginFrom'
+import { useApolloClient, useQuery } from '@apollo/client/react'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const [token, setToken] = useState(localStorage.getItem('library-user-token'))
+  const client = useApolloClient()
+
+  const onLogout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  if (!token) {
+    return (
+      <div>
+        <div>
+          <button onClick={() => setPage('authors')}>authors</button>
+          <button onClick={() => setPage('books')}>books</button>
+          <button onClick={() => setPage('login')}>login</button>
+        </div>
+
+        <Authors show={page === 'authors'} />
+
+        <Books show={page === 'books'} />
+
+        <LoginForm setToken={setToken} show={page === 'login'} />
+      </div>
+    )
+  }
 
   return (
-    <div>
       <div>
-        <button onClick={() => setPage('authors')}>authors</button>
-        <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+        <div>
+          <button onClick={() => setPage('authors')}>authors</button>
+          <button onClick={() => setPage('books')}>books</button>
+          <button onClick={() => setPage('add')}>add book</button>
+          <button onClick={onLogout}>logout</button>
+        </div>
+
+        <Authors show={page === 'authors'} />
+
+        <Books show={page === 'books'} />
+
+        <NewBook show={page === 'add'} />
       </div>
-
-      <Authors show={page === 'authors'} />
-
-      <Books show={page === 'books'} />
-
-      <NewBook show={page === 'add'} />
-    </div>
-  )
+    )
+  
 }
 
 export default App
