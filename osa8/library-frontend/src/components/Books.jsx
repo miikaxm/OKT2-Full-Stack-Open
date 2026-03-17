f1cc07aa8b7e979ee021e139cf15c35ccd7fe1b3
Miika Valkonen <miika.valkonen08@gmail.com>
2026-03-17 10:30:38 +0200

    8.19 Genren kirjat

--- osa8/library-frontend/src/components/Books.jsx
+++ osa8/library-frontend/src/components/Books.jsx
@@ -1,17 +1,25 @@
 import { ALL_BOOKS } from "../queries"
 import { useQuery } from '@apollo/client/react'
+import { useState } from "react"
+
+
 
 const Books = (props) => {
   if (!props.show) {
     return null
   }
 
+  const [filter, setFilter] = useState('')
   const { loading, error, data } = useQuery(ALL_BOOKS)
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error: {error.message}</p>
 
   const books = data.allBooks
+
+  const filteredBooks = filter
+  ? books.filter((b) => b.genres.includes(filter))
+  : books
 
   return (
     <div>
@@ -24,7 +32,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((b) => (
+          {filteredBooks.map((b) => (
             <tr key={b.id}>
               <td>{b.title}</td>
               <td>{b.author.name}</td>
@@ -33,6 +41,13 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      <button onClick={() => setFilter('refactoring')}>refactoring</button>
+      <button onClick={() => setFilter('agile')}>agile</button>
+      <button onClick={() => setFilter('patterns')}>patterns</button>
+      <button onClick={() => setFilter('design')}>design</button>
+      <button onClick={() => setFilter('crime')}>crime</button>
+      <button onClick={() => setFilter('classic')}>classic</button>
+      <button onClick={() => setFilter('')}>all genres</button>
     </div>
   )
 }
